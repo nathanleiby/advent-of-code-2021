e35bd47ad5af89eb0dfe1066be2c1f997dd88510
Nathan Leiby <nathanleiby@gmail.com>
2021-12-02 00:49:06 -0800

    day01 - part2

--- day01/src/main.zig
+++ day01/src/main.zig
@@ -13,16 +13,37 @@ pub fn main() anyerror!void {
     var buf: [MAX_LINE_LENGTH]u8 = undefined;
 
     var numIncreased: i32 = 0;
-    var prev: i32 = -1;
+
+    // we use -1 as a sentinel value
+    // assumption: values in the text input are not == -1
+    // TODO: Could try using undefined in zig
+    var prev2: i32 = -1;
+    var prev1: i32 = -1;
+    var cur: i32 = -1;
+
+    var prevWindowSum: i32 = -1;
 
     while (try in_stream.readUntilDelimiterOrEof(&buf, '\n')) |line| {
-        // do something with line...
-
         const val = try std.fmt.parseInt(i32, line, 10);
-        if (prev != -1 and val > prev) {
-            numIncreased = numIncreased + 1;
+
+        // move the window
+        prev2 = prev1;
+        prev1 = cur;
+        cur = val;
+
+        // compute window sum
+        var curWindowSum: i32 = -1;
+        if (prev2 != -1 and prev1 != -1 and cur != -1) {
+            curWindowSum = prev2 + prev1 + cur;
         }
-        prev = val;
+
+        if (prevWindowSum != -1) {
+            if (curWindowSum > prevWindowSum) {
+                numIncreased += 1;
+            }
+        }
+
+        prevWindowSum = curWindowSum;
     }
 
     std.log.info("num increased: {d}", .{numIncreased});
